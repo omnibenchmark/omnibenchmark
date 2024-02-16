aa5aad47a61fae4663b06902f1b7e4a6cf874776
almutlue <almut.lue@gmail.com>
2024-02-16 12:00:21 +0100

    chore: dataset code signatures

---
+++ omni_py/datasets.py
@@ -0,0 +1,14 @@
+"""Functions to manage dataset handling"""
+
+def list_datasets(bench_name: str, version: str, stage:str):
+    """List all available datasets for a certain benchmark, version and stage"""
+    pass
+
+def mount_dataset(url:str):
+    """Mount a specific dataset based on its url"""
+    # Do we download/store/cache locally here? 
+    pass
+
+def get_test_dataset(bench_name: str, version: str, stage:str):
+    """Mount a test dataset for a certain benchmark version and stage"""
+    pass
--- omni_py/sync.py
+++ omni_py/sync.py
@@ -7,17 +7,20 @@ from .config import bench_dir
 
 base = "https://github.com/"
 omni_essentials = "omnibenchmark/omni_essentials"
+bench_definition = "benchmark_definition.yaml"
 
 benchmarks_json = "-/raw/master/data/data_infos.json?inline=false"
 orchestrator_json = "-/raw/master/data/orchestrator_infos.json?inline=false"
 
 
-def download_bench_definition(bench_name: str, version: str, force: bool =False):
-    bench_yaml = f"{bench_name}/{version}/benchmark_definition.yaml"
+def download_bench_definition(bench_name: str, version: str, force: bool = False):
+    bench_yaml = f"{bench_name}/{version}/{bench_definition}"
     local_bench_yaml = bench_dir + bench_yaml
-    remote_bench_yaml = base + omni_essentials + f"-/raw/master/benchmarks/{bench_yaml}?inline=false"
+    remote_bench_yaml = (
+        base + omni_essentials + f"-/raw/master/benchmarks/{bench_yaml}?inline=false"
+    )
     if force or not os.path.isfile(local_bench_yaml):
         r = requests.get(remote_bench_yaml)
         data = r.json()
-        with open(local_bench_yaml, 'w') as f:
+        with open(local_bench_yaml, "w") as f:
             json.dump(data, f)
