5a1580188ab6382a7867ab2eafd2c43630b7f025
Reto Gerber <reto.gerber@uzh.ch>
2024-05-28 13:22:46 +0200

    implement list_benchmarks
    Does implement https://github.com/orgs/omnibenchmark/projects/2/views/1?pane=issue&itemId=63534208

--- omni/cli/benchmark.py
+++ omni/cli/benchmark.py
@@ -3,6 +3,10 @@
 from typing_extensions import Annotated
 
 import typer
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import requests
+import re
 
 cli = typer.Typer(add_completion=False)
 
@@ -81,6 +85,36 @@ def list_benchmarks(
 ):
     """List all available benchmarks and versions at a specific endpoint"""
     typer.echo(f"Available benchmarks at {endpoint}:")
+    url = urlparse(f"{endpoint}/benchmarks")
+    response = requests.get(url.geturl(), params={"format": "xml"})
+    if response.ok:
+        response_text = response.text
+    else:
+        response.raise_for_status()
+    soup = BeautifulSoup(response_text, "xml")
+    benchmark_names = [obj.find("Key").text for obj in soup.find_all("Contents")]
+    benchmarks = {}
+    for benchmark in benchmark_names:
+        url = urlparse(f"{endpoint}/{benchmark}.overview")
+        response = requests.get(url.geturl(), params={"format": "xml"})
+        if response.ok:
+            soup = BeautifulSoup(response.text, "xml")
+            buckets = [obj.find("Key").text for obj in soup.find_all("Contents")]
+            versions = []
+            for bucket in buckets:
+                if re.search(r"(\d+\.\d+)", bucket):
+                    versions.append(bucket)
+            from packaging.version import Version
+
+            versions.sort(key=Version)
+            if isinstance(versions, list) and len(versions) > 0:
+                benchmarks[benchmark] = versions
+            elif isinstance(versions, str) and len(versions) > 0:
+                benchmarks[benchmark] = [versions]
+            else:
+                benchmarks[benchmark] = [""]
+    for key, value in sorted(benchmarks.items()):
+        typer.echo(f"{key:>20} {value[-1]:>8}")
 
 
 @cli.command("list versions")
---
+++ tests/io/test_list_benchmarks.py
@@ -0,0 +1,25 @@
+import omni.cli.benchmark as ocb
+import json
+import os
+from pathlib import Path
+import pytest
+
+if Path("tests/.minio_test_config.json").exists():
+    with open("tests/.minio_test_config.json", "r") as file:
+        auth_options = json.load(file)
+elif "S3_ENDPOINT_URL" in os.environ:
+    auth_options = {}
+    auth_options["endpoint"] = os.environ.get("S3_ENDPOINT_URL")
+    auth_options["secure"] = False
+else:
+    raise ValueError("No S3 credentials found")
+
+endpoint = (
+    f"http://{auth_options['endpoint'].replace('http://', '').replace('https://', '')}"
+)
+
+
+def test_list_benchmark(capfd):
+    ocb.list_benchmarks(endpoint)
+    out, err = capfd.readouterr()
+    assert len(out) > 0
