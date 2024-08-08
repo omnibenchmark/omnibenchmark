2b5c55fbed0cda2ffeb820de091a4b011f1d3626
Daniel Incicau <daniel.incicau@gmail.com>
2024-08-08 12:23:49 +0200

    Deal with permission erorr on Windows when doing tests cleanup

--- tests/workflow/Snakemake_setup.py
+++ tests/workflow/Snakemake_setup.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import stat
 from pathlib import Path
 
 from omni.benchmark import Benchmark
@@ -33,7 +34,12 @@ class SnakemakeSetup:
                 if os.path.isfile(file_path):
                     os.remove(file_path)
                 elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
+                    shutil.rmtree(file_path, onerror=self._remove_readonly)
+
+    def _remove_readonly(self, func, path, _):
+        "Clear the readonly bit and reattempt the removal"
+        os.chmod(path, stat.S_IWRITE)
+        func(path)
 
     def _print_benchmark(self):
         converter = self.benchmark.get_converter()
