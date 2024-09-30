86ca430ceb7eaf909f4e1f83bc0253bba9005660
Daniel Incicau <daniel.incicau@gmail.com>
2024-09-30 10:47:24 +0200

    Fix failing run with software tests

--- tests/workflow/test_run_workflow.py
+++ tests/workflow/test_run_workflow.py
@@ -30,19 +30,3 @@ def test_run_workflow_002():
 
         success = setup.workflow.run_workflow(benchmark)
         assert success
-
-
-def test_run_mock_benchmark_with_software():
-    benchmark_file = Path("..") / "data" / "mock_benchmark.yaml"
-    benchmark_file_path = Path(__file__).parent / benchmark_file
-
-    with SnakemakeSetup(benchmark_file_path) as setup:
-        benchmark = setup.benchmark
-        assert benchmark.get_benchmark_name() == "mock_benchmark"
-
-        backend = benchmark.get_benchmark_software_backend()
-
-        # First run the whole workflow
-        success = setup.workflow.run_workflow(benchmark, backend=backend)
-
-        assert success
