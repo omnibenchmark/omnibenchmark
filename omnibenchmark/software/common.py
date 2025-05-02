e5d09e5aefb1e687243573140dc9bbec15d3a8a6
ben <ben.uzh@proton.me>
2025-05-02 14:03:28 +0200

    bug: do not raise exception when checking software envs. Closes: #97
    since the CompletedProcess is checked for the return code, we don't want
    to raise an exception since that's not catched by the click invocation.

--- omnibenchmark/software/common.py
+++ omnibenchmark/software/common.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python
-
 """
 Common omniblock software management logics, regardless of conda/easybuild/system.
 Calls workflow capabilities to reuse schema / yaml parsing logics
 """
 
-# import omni_workflow import main
-
 import subprocess
 
 
 def check_call(command: str, use_shell: bool = True) -> subprocess.CompletedProcess:
-    ret = subprocess.run(
+    return subprocess.run(
         command,
         text=True,
         capture_output=True,
-        check=True,
+        check=False,
         shell=use_shell,
     )
-    return ret
 
 
 def check_lmod_status() -> subprocess.CompletedProcess:
-    # return check_call("type module", use_shell=True)
     ret = subprocess.run(
         ["type", "module"], text=True, capture_output=True, check=False, shell=True
     )
@@ -39,8 +33,3 @@ def check_easybuild_status() -> subprocess.CompletedProcess:
 
 def check_conda_status() -> subprocess.CompletedProcess:
     return check_call("conda --version")
-
-
-# ## not used, snakemake cannot use docker containers directly
-# def check_docker_status() -> subprocess.CompletedProcess:
-#     return check_call("docker --version")
