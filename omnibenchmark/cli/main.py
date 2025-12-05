67b934839c2c990c8a82014b88144bdfbbf40a46
Daniel Incicau <daniel.incicau@gmail.com>
2025-12-05 19:11:04 +0100

    Update parse_performance.py

--- omnibenchmark/cli/main.py
+++ omnibenchmark/cli/main.py
@@ -8,6 +8,7 @@ from omnibenchmark.cli.io import storage
 from omnibenchmark.cli.create import create
 from omnibenchmark.cli.run import run
 from omnibenchmark.cli.status import status
+from omnibenchmark.cli.dashboard import dashboard
 
 from .debug import add_debug_option
 
@@ -70,6 +71,7 @@ cli.add_command(add_debug_option(run))
 cli.add_command(add_debug_option(info))
 cli.add_command(add_debug_option(create))
 cli.add_command(add_debug_option(status))
+cli.add_command(add_debug_option(dashboard))
 
 add_debug_option(cli)
 
